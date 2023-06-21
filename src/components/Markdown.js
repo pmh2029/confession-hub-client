51dbf5e40061599ec42fd6ad70aabaa7348234a8
pmh2029 <hieu.pm2029@gmail.com>
2023-06-22 03:21:38 +0800

    add react-markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,6 +1,13 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { monokaiSublime } from "reacgt-syntax-highlighter/dist/esm/styles/hljs";
+
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
 import "./markdown.css";
 
 const Markdown = ({ content }) => {
@@ -12,24 +19,60 @@ const Markdown = ({ content }) => {
         className="markdown"
         style={{ "&p": { margin: 0 } }}
         disallowedElements={disallowed}
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeKatex]}
         skipHtml={false}
         children={content}
         components={{
           img: ({ node, ...props }) => (
-            <img style={{ maxWidth: "450px" }} {...props} />
+            <img style={{ maxWidth: "450px", margin: "5px" }} {...props} />
           ),
           pre: ({ node, ...props }) => (
             <pre
               style={{
                 backgroundColor: "#DCDCDC",
                 maxWidth: "450px",
-                marginBlock: "5px",
-                wordWrap: "break-word",
+                margin: "5px",
               }}
               {...props}
             />
           ),
-          a: ({ node, ...props }) => <a target="_blank" {...props} />,
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, "")}
+                style={monokaiSublime}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+          p: ({ node, ...props }) => (
+            <p
+              style={{
+                wordBreak: "break-word",
+                maxWidth: "450px",
+                margin: "5px",
+              }}
+              {...props}
+            />
+          ),
+          a: ({ node, ...props }) => (
+            <a
+              target="_blank"
+              style={{
+                color: "#1976d2",
+                cursor: "pointer",
+              }}
+              {...props}
+            />
+          ),
         }}
       />
     </Typography>
